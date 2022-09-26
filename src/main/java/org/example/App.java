f9052ac36f6e21984cc29dc7d9b278017427e338
tureiko <antonina03032007>
2022-09-26 21:59:11 +0300

    модуль2.1

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class App {
+ public static void main(String[] args) {
+
+Transport.getPrice(100);
+//  public void setCity(int city) {
+ //     this.city = city; Transport.getPrice(100);}
+ }
+
+}
---
+++ src/main/java/org/example/Transport.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Transport {
+    private String name="фургон";
+    private int capacity=500;
+    private int speed=90;
+
+
+    public static float getPrice(int city){
+        double costOfKm=3.2;
+        double stoimost= 100 *costOfKm;
+        System.out.println(stoimost);
+
+
+        return 0;
+    }
+
+}
+
+
+
+
