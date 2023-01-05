c8de21f03b6c79a111916a0469696d9f893e3183
tureiko <antonina03032007@yandex.ru>
2023-01-05 17:44:55 +0300

    TransportFactory update

--- src/main/java/org/example/TransportFactory.java
+++ src/main/java/org/example/TransportFactory.java
@@ -3,7 +3,7 @@ package org.example;
 
 public class TransportFactory {
 
-    Transport getTransport(City city, int weight, int hours) {
+    public Transport getTransport(City city, int weight, int hours) {
         final String SHIP_NAME = "Корабль";
         final String TRUCK_NAME = "Грузовик";
         final String PLANE_NAME = "Самолёт";
@@ -11,36 +11,30 @@ public class TransportFactory {
         final float TRUCK_PRICE = 10.03f;
         final float PLANE_PRICE = 50.01f;
 
-        Transport newTransport;
-        int speed = city.getDistance() / hours;// скорость с которой должен двигаться транспорт для доставки в срок
-
-        if (speed < 40 && city.isOnWater()) {
-            newTransport = new Ship(SHIP_NAME, rWeight(weight), rSpeed(speed), SHIP_PRICE);
-        } else if (speed > 120 && city.isHasAirport()) {
-            newTransport = new Plane(PLANE_NAME, rWeight(weight), rSpeed(speed), PLANE_PRICE);
+        int reqSpeed = city.getDistance() / hours;// скорость с которой должен двигаться транспорт для доставки в срок
+        if (reqSpeed < 40 && city.isOnWater()) {
+            System.out.println("Ship");
+            return new Ship(SHIP_NAME, roundWeight(weight), roundSpeed(reqSpeed), SHIP_PRICE);
         }
-        else {
-            newTransport = new Truck(TRUCK_NAME, rWeight(weight),rSpeed(speed), TRUCK_PRICE);
+        if (reqSpeed > 120 && city.isHasAirport()) {
+            System.out.println("Plane");
+            return new Plane(PLANE_NAME, roundWeight(weight), roundSpeed(reqSpeed), PLANE_PRICE);
         }
+        System.out.println("Truck");
+        return new Truck(TRUCK_NAME, roundWeight(weight), roundSpeed(reqSpeed), TRUCK_PRICE);
 
-        System.out.println(newTransport.getName()+" - грузоподъёмность "+ newTransport.getCapacity()+" скорость "+ newTransport.getSpeed());
-        return newTransport;
     }
 
-    public int rSpeed(int spd){  // метод для округления скорости
-        if (spd%10 ==0) return spd;
-        else {
-            int b= (int) (Math.ceil(spd/10.0)*10);
-            return  b;
-        }
+    private int roundSpeed(int speed) {  // метод для округления скорости
+        if (speed % 10 == 0) return speed;
+        return (int) (Math.ceil(speed / 10.0) * 10);
     }
 
-    public static int rWeight(int a){  // необходимая грузоподъемность
-        if (a % 500 == 0) {return a;}
-        else {
-            int b= (int) (Math.ceil(a/500.0)*500);
-            return  b;
+    private static int roundWeight(int weight) {  // метод для округления грузоподъемности
+        if (weight % 500 == 0) {
+            return weight;
         }
+        return (int) (Math.ceil(weight / 500.0) * 500);
     }
 
 }
